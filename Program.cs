febe2a920f5b4f1979150a72c69fafff4ebe9884
Troels Lund <trolund@gmail.com>
2020-04-15 10:11:41 +0200

    port 4000

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace WebApi {
             Host.CreateDefaultBuilder (args)
             .ConfigureWebHostDefaults (webBuilder => {
                 webBuilder.UseStartup<Startup> ()
-                    .UseUrls ("http://localhost:80");
+                    .UseUrls ("http://localhost:4000");
             });
     }
 }
