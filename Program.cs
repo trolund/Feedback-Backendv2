59c9a8537219f951b72228930d5e09c7c9defd2f
Troels Lund <trolund@gmail.com>
2020-04-16 11:57:03 +0200

    test

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace WebApi {
             Host.CreateDefaultBuilder (args)
             .ConfigureWebHostDefaults (webBuilder => {
                 webBuilder.UseStartup<Startup> ()
-                    .UseUrls ("https://*:4000");
+                    .UseUrls ("http://*:4000");
             });
     }
 }
